d50d55bf6caadd941bc9bd62983f78176d580371
Richard Bobo <richard.bobo@gmail.com>
2020-03-02 02:17:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,9 @@ def adding_matz
   
 
    
-     programmer_hash[:adding_matz] => {:yukihiro_matsumoto = {:known_for=> "Ruby", :languages => ["LISP", "C"]}}
+     programmer_hash[:adding_matz] => {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}
+       
+     }
 
 end
 
